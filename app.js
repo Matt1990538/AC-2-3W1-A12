2d02e78615da56a255e5eed01bc8bffc95d774c5
m.chaohsunt <m.chaohsunt@gmail.com>
2021-10-10 17:19:24 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 const express = require('express')
-const app = express()
-const port = 3000
-
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 const methodOverride = require('method-override')
 
+const port = process.env.port || 3000
+
 const Restaurant = require('./models/restaurant')
 const routes = require('./routes')
 
-// database connection
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/Restaurant_List', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = require('./config/mongoose')
 
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const app = express()
 
 // template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,16 +1,9 @@
-const mongoose = require('mongoose')
 const restaurant = require('../restaurant') 
 const restaurantList = require('../../restaurant.json')
 
-mongoose.connect('mongodb://localhost/Restaurant_List', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = require('../../config/mongoose')
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
-
   for (let i = 0; i <= restaurantList.results.length - 1; i++) {
 
     restaurant.create({ 
